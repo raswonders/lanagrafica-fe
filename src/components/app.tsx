4d8cbcf562d3d2b74c0be0ecedf571d30be65ae7
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-03-10 15:34:18 +0100

    display test-bg only in dark mode

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,18 +1,20 @@
 import { SignIn } from "./sign-in";
-import { ThemeProvider } from "./theme-provider";
 import { ModeToggle } from "./mode-toggle";
 import bgImage from "/public/test-bg.svg";
+import { useTheme } from "./theme-provider";
 
 function App() {
+  const { theme } = useTheme();
+
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div style={{ backgroundImage: `url(${bgImage})`}}>
-        <div className="absolute m-4">
-          <ModeToggle />
-        </div>
-        <SignIn />
+    <div
+      style={theme === "light" ? {} : { backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="absolute m-4">
+        <ModeToggle />
       </div>
-    </ThemeProvider>
+      <SignIn />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/app.tsx";
 import "normalize.css";
 import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
