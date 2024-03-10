615cb45490732904eba7f3ff451fcbaef716a886
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-03-10 14:57:50 +0100

    add test-bg

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,17 @@
 import { SignIn } from "./sign-in";
 import { ThemeProvider } from "./theme-provider";
 import { ModeToggle } from "./mode-toggle";
+import bgImage from "/public/test-bg.svg";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="absolute m-4">
-        <ModeToggle />
+      <div style={{ backgroundImage: `url(${bgImage})`}}>
+        <div className="absolute m-4">
+          <ModeToggle />
+        </div>
+        <SignIn />
       </div>
-      <SignIn />
     </ThemeProvider>
   );
 }
