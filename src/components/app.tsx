919e763b05e60b7bc08cd6298977647db0dddc47
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-03-12 15:31:18 +0100

    make bg fixed rather than absolute

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -23,7 +23,7 @@ function App() {
     <>
       <Background
         style={{
-          position: "absolute",
+          position: "fixed",
           width: "100%",
           height: "100%",
           zIndex: "-9999",
