39dd195907ee21c369ccbd3e382df7ca5e32f098
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-03-12 16:38:42 +0100

    conditionally render private parts of navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,55 +1,60 @@
 import { AccountDetails } from "./account-details";
+import { useAuth } from "./auth-provider";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
 import { NavLink } from "react-router-dom";
 
 export function Navbar() {
+  const { user } = useAuth();
+
   return (
     <div className="border-neutral-6 flex w-full justify-between p-3">
       <ModeToggle />
 
       <nav>
-        <ul className="flex gap-3">
-          <li>
-            <NavLink to={"/home"}>
-              {({ isActive }) => (
-                <Button
-                  variant="ghost"
-                  className={isActive ? "text-neutral-12 bg-accent-a3" : ""}
-                >
-                  Home
-                </Button>
-              )}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/members"}>
-              {({ isActive }) => (
-                <Button
-                  variant="ghost"
-                  className={isActive ? "text-neutral-12 bg-accent-a3" : ""}
-                >
-                  Members
-                </Button>
-              )}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/cards"}>
-              {({ isActive }) => (
-                <Button
-                  variant="ghost"
-                  className={isActive ? "text-neutral-12 bg-accent-a3" : ""}
-                >
-                  Cards
-                </Button>
-              )}
-            </NavLink>
-          </li>
-        </ul>
+        {user && (
+          <ul className="flex gap-3">
+            <li>
+              <NavLink to={"/home"}>
+                {({ isActive }) => (
+                  <Button
+                    variant="ghost"
+                    className={isActive ? "text-neutral-12 bg-accent-a3" : ""}
+                  >
+                    Home
+                  </Button>
+                )}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={"/members"}>
+                {({ isActive }) => (
+                  <Button
+                    variant="ghost"
+                    className={isActive ? "text-neutral-12 bg-accent-a3" : ""}
+                  >
+                    Members
+                  </Button>
+                )}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={"/cards"}>
+                {({ isActive }) => (
+                  <Button
+                    variant="ghost"
+                    className={isActive ? "text-neutral-12 bg-accent-a3" : ""}
+                  >
+                    Cards
+                  </Button>
+                )}
+              </NavLink>
+            </li>
+          </ul>
+        )}
       </nav>
 
-      <AccountDetails />
+      {user && <AccountDetails user={user} />}
     </div>
   );
 }
