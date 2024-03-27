c6dfd4b7853ddbb9daa2b13894f1f2b37dce7d8f
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-03-27 18:02:56 +0100

    make navbar static

--- src/components/layouts/page-layout.tsx
+++ src/components/layouts/page-layout.tsx
@@ -8,7 +8,7 @@ type PagePropsProps = {
 
 export function PageLayout({ title, children }: PagePropsProps) {
   return (
-    <section className="sm:px-8 pt-24 ">
+    <section className="sm:px-8">
       <Card>
         <CardHeader>
           <CardTitle>{title}</CardTitle>
