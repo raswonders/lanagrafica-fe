f07c6655260991cc4680a01785739f5b0397efa6
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-03-27 13:17:00 +0100

    add logo and conditionally render dark mode toggle

---
+++ src/components/logo.tsx
@@ -0,0 +1,3 @@
+export function Logo() {
+  return <div className="p-2">Logo</div>
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,7 @@ import { Button } from "./ui/button";
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { UserPlus } from "lucide-react";
+import { Logo } from "./logo";
 
 export function Navbar() {
   const { user } = useAuth();
@@ -12,25 +13,13 @@ export function Navbar() {
 
   return (
     <div className="fixed border-neutral-6 flex w-full justify-between p-3">
-      <ModeToggle />
+      <NavLink to={"/"}>
+        <Logo />
+      </NavLink>
 
       <nav>
         {user && (
           <ul className="flex gap-3">
-            <li>
-              <NavLink to={"/"} tabIndex={-1}>
-                {({ isActive }) => (
-                  <Button
-                    variant="ghost"
-                    className={
-                      isActive ? "text-accent-foreground bg-accent" : ""
-                    }
-                  >
-                    {t("navbar.home")}
-                  </Button>
-                )}
-              </NavLink>
-            </li>
             <li>
               <NavLink to={"/members"} tabIndex={-1}>
                 {({ isActive }) => (
@@ -60,25 +49,11 @@ export function Navbar() {
                 )}
               </NavLink>
             </li>
-            <li>
-              <NavLink to={"/cards"} tabIndex={-1}>
-                {({ isActive }) => (
-                  <Button
-                    variant="ghost"
-                    className={
-                      isActive ? "text-accent-foreground bg-accent" : ""
-                    }
-                  >
-                    {t("navbar.cards")}
-                  </Button>
-                )}
-              </NavLink>
-            </li>
           </ul>
         )}
       </nav>
 
-      {user && <AccountDetails />}
+      {user ? <AccountDetails /> : <ModeToggle />}
     </div>
   );
 }
