2a5da83ff862239a7078d15db6d23b5f9a626281
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-04-03 20:14:22 +0200

    add new dummy data

--- src/components/members-table.tsx
+++ src/components/members-table.tsx
@@ -14,67 +14,24 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import rawData from "../assets/members-test.json";
+const data = rawData.map(({ surname, ...row }) => ({
+  ...row,
+  name: `${row.name} ${surname}`,
+}));
+
 type Member = {
-  id: string;
   name: string;
+  province: string;
+  birthDate: string;
+  birthPlace: string;
   email: string;
-  active: boolean;
+  docType: string;
+  docId: string;
+  country: string;
 };
 
-const dummyData: Member[] = [
-  { id: "1", name: "John Doe", email: "john.doe@example.com", active: true },
-  { id: "2", name: "Jane Doe", email: "jane.doe@example.com", active: false },
-  { id: "3", name: "Bob Smith", email: "bob.smith@example.com", active: true },
-  {
-    id: "4",
-    name: "Alice Johnson",
-    email: "alice.johnson@example.com",
-    active: false,
-  },
-  {
-    id: "5",
-    name: "Charlie Brown",
-    email: "charlie.brown@example.com",
-    active: true,
-  },
-  {
-    id: "6",
-    name: "Megan Davis",
-    email: "megan.davis@example.com",
-    active: false,
-  },
-  {
-    id: "7",
-    name: "Tom Thompson",
-    email: "tom.thompson@example.com",
-    active: true,
-  },
-  {
-    id: "8",
-    name: "Sophia Martinez",
-    email: "sophia.martinez@example.com",
-    active: false,
-  },
-  {
-    id: "9",
-    name: "Michael Robinson",
-    email: "michael.robinson@example.com",
-    active: true,
-  },
-  {
-    id: "10",
-    name: "Grace Cox",
-    email: "grace.cox@example.com",
-    active: false,
-  },
-];
-const data = dummyData;
-
 const columns: ColumnDef<Member>[] = [
-  {
-    accessorKey: "id",
-    header: "Id",
-  },
   {
     accessorKey: "name",
     header: "Name",
@@ -82,10 +39,6 @@ const columns: ColumnDef<Member>[] = [
   {
     accessorKey: "email",
     header: "Email",
-  },
-  {
-    accessorKey: "active",
-    header: "Active",
   },
 ];
 
