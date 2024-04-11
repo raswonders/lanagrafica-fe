ffb584cdebd43f0e0b448aa15fc89c24c54e15a2
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-04-11 19:22:33 +0200

    add supabase client connection

---
+++ src/components/supabase.tsx
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://anlhkcaoszuqiatipxjr.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFubGhrY2Fvc3p1cWlhdGlweGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2ODE2NDYsImV4cCI6MjAyODI1NzY0Nn0.GQAGYJnpI0tS9cKqiE0ac3z9c_2iOFwcSC35qV1heyc";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
