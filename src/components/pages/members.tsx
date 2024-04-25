a5f71e67ddfdcc9b12d3a0262df2d879423db551
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-04-25 14:20:20 +0200

    add search bar

--- src/components/pages/members.tsx
+++ src/components/pages/members.tsx
@@ -3,15 +3,34 @@ import { PageLayout } from "../layouts/page-layout";
 import { useTranslation } from "react-i18next";
 import { DataTable } from "../members-table";
 import { Separator } from "@/components/ui/separator";
+import { Input } from "../ui/input";
+import { Search } from "lucide-react";
+import { useState } from "react";
 
 export function Members() {
   const { t } = useTranslation();
+  const [search, setSearch] = useState("");
 
   return (
     <PageLayout>
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-row items-end justify-between gap-6">
           <CardTitle>{t("members.title")}</CardTitle>
+          <div className="w-[350px] relative">
+            <Input
+              id="searchInput"
+              placeholder={t("members.searchPlaceholder")}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="pl-10"
+            />
+            <label
+              htmlFor="searchInput"
+              className="absolute left-3 top-1/2 transform -translate-y-1/2"
+            >
+              <Search className="w-5 text-accent-11" />
+            </label>
+          </div>
         </CardHeader>
         <CardContent>
           <Separator />
