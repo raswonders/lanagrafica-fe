a88ab73b9273390b7f2aff897c41d08f1b2bd978
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-05-23 19:11:40 +0200

    style cross in sheet

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,6 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { Button } from "./button";
 
 const Sheet = SheetPrimitive.Root;
 
@@ -63,7 +64,7 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm ring-offset-background transition-opacity text-accent-11 hover:text-accent-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-8 w-8 sm:mr-1 sm:h-6 sm:w-6" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
