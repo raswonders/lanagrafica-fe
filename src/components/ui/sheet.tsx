813d67009a09073f409d65726a92d45efea26a0a
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-05-23 19:15:25 +0200

    change sheet background

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -30,7 +30,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+  "fixed z-50 gap-4 bg-neutral-6 dark:bg-neutral-2 dark:border-neutral-4 text-neutral-12 p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
   {
     variants: {
       side: {
