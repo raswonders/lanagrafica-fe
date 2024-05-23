b3264404955c4102578bcd73720602ad72c3fd3e
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-05-23 19:41:26 +0200

    remove unnecessary Button import

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,7 +4,6 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { Button } from "./button";
 
 const Sheet = SheetPrimitive.Root;
 
