fc1b1a1237909f1e07023c2f80b6b9049b7fc612
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-11-15 19:17:06 +0100

    add style to logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,3 +1,8 @@
 export function Logo() {
-  return <div className="p-2">Logo</div>;
+  return (
+    <div className="p-2 flex flex-col items-center text-sm font-semibold text-accent-foreground">
+      <span>CLUB</span>
+      <span className="text-accent-10">IOI</span>
+    </div>
+  );
 }
