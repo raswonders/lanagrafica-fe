82bfb27cfe3fd6a277affe924b11ea1f602968c5
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-11-20 17:45:25 +0100

    test adding rows on scroll

--- tests/members-table.spec.ts
+++ tests/members-table.spec.ts
@@ -1,11 +1,21 @@
 import { test, expect } from "@playwright/test";
 
 test("renders rows", async ({ page }) => {
+  // renders rows on navigation
   await page.goto("/");
-
   const rows = page.locator('tr[data-row="true"]');
   await rows.first().waitFor();
+  const initialRowCount = await rows.count();
+  expect(initialRowCount).toBeGreaterThan(0);
 
-  const rowCount = await rows.count();
-  expect(rowCount).toBeGreaterThan(0);
+  // renders rows on scroll
+  await page.evaluate(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+  await page
+    .locator('tr[data-row="true"]')
+    .nth(initialRowCount + 1)
+    .waitFor();
+  const afterScrollCount = await rows.count();
+  expect(afterScrollCount).toBeGreaterThan(initialRowCount);
 });
