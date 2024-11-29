f583dbb2c97e2aaf509d923a9eae71fa84207743
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-11-29 20:20:26 +0100

    add initiallization script for members table

---
+++ test-server/init-members.ts
@@ -0,0 +1,13 @@
+import { sql } from "./db";
+import {
+  createMembersSnapshot,
+  createMembersTable,
+  insertMembersData,
+  updateIsActive,
+} from "./table";
+
+await createMembersTable();
+await insertMembersData();
+await updateIsActive();
+await createMembersSnapshot();
+await sql.end();
