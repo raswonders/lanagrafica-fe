e4a0107f101cdb5155401a408e3eebe609073e9a
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-11-29 19:58:53 +0100

    add createMembersTable fn

--- test-server/table.ts
+++ test-server/table.ts
@@ -75,3 +75,45 @@ export async function updateIsActive() {
     await sql.end();
   }
 }
+
+export async function createMembersTable() {
+  try {
+    await sql`
+    DROP TABLE IF EXISTS members;
+  `;
+
+    await sql`
+      CREATE TABLE members (
+        id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        birth_date TIMESTAMP WITH TIME ZONE NOT NULL,
+        birth_place TEXT,
+        card_number TEXT,
+        country TEXT,
+        doc_id TEXT,
+        doc_type TEXT,
+        email TEXT,
+        expiration_date TIMESTAMP WITH TIME ZONE,
+        is_active BOOLEAN,
+        is_deleted BOOLEAN,
+        measure TEXT,
+        name TEXT,
+        note TEXT,
+        province TEXT,
+        registration_date TIMESTAMP WITH TIME ZONE,
+        surname TEXT,
+        suspended_till TIMESTAMP WITH TIME ZONE,
+        name_surname TEXT GENERATED ALWAYS AS (name || ' ' || surname) STORED
+      );
+    `;
+    console.log('Table "members" created successfully!');
+
+    await sql`
+      CREATE INDEX idx_name_surname ON members USING GIN (to_tsvector('italian', name_surname));
+    `;
+    console.log('Index "idx_name_surname" created successfully!');
+  } catch (error) {
+    console.error("Error creating table or index:", error);
+  } finally {
+    await sql.end();
+  }
+}
